6c86b05f48e0eeeddf560221a6fd26bccb233162
Sean Connelly <sconnel42@gmail.com>
2019-01-12 15:45:36 -0600

    Page init with Vue and Bootstrap

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    messages: [
+      'Hello Vue.js Mark 3!',
+      'Where is my super suit?',
+      'Gonna Getcha Cotton Eyed Joe'
+    ]
+  }
+})
