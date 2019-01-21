0a43658588cf0e7dcde549c390bdb8ab645ef77f
Sean Connelly <sconnel42@gmail.com>
2019-01-21 13:31:04 -0600

    Add RSVP Form

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 A website for our wedding, written with Vue and Bootstrap
 
 ### TODO:
-- Form for RSVP
-- Get images for Gallery
+
+#### Frontend
 - Contact Form
+- Links to Registries
 - Write actual content in About Us
+- Get images for Gallery
+- Send RSVP form info to mock BE endpoint
 - Figure out what colors we want to use
 - Make sections collapable?
     - If so, then the nav bar is probably not needed
@@ -17,3 +20,13 @@ A website for our wedding, written with Vue and Bootstrap
     - Make Date bigger
     - Change Date font
     - Date on image?
+- Fix styling for RSVP form
+
+
+#### Backend
+- Create BE server (Express?)
+- Serve wedding website
+- Write RSVPs to Database
+- Host site on netlify
+
+- Stats page on who has RSVPed?
--- index.js
+++ index.js
@@ -27,11 +27,35 @@ var app = new Vue({
         alt: 'Skiing in Lutsen 2019'
       },
     ],
+    mealOptions: [
+      "Chicken",
+      "Pork"
+    ],
     imageCols: 3,
+    rsvpFormUrl: 'http://localhost:9000/submit-rsvp',
+    rsvpData: {
+      name: '',
+      email: '',
+      meal: 'Chicken',
+      isComing: true
+    }
   },
   computed: {
     imageRows: function () {
       return Math.ceil(this.images.length/this.imageCols);
+    }
+  },
+  methods: {
+    handleRSVPSubmit() {
+      if(this.rsvpData.isComing === true) {
+        console.log(this.rsvpData.name, "is coming to the wedding");
+        console.log(this.rsvpData.name, "will have", this.rsvpData.meal, "for dinner");
+      }
+      else {
+        console.log(this.rsvpData.name, "is not coming to the wedding");
+      }
+      console.log(this.rsvpData.name, "is using this email:", this.rsvpData.email);
+      console.log("Sending RSVP for", this.rsvpData.name, "to", this.rsvpFormUrl);
     }
   }
 });
