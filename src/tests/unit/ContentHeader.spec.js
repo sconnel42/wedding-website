8cef7b5e7e940fe588ffa666bf4c4fd8230f2155
Sean Connelly <sconnel42@gmail.com>
2019-08-05 19:58:01 -0500

    Tests for ContentHeader

--- src/tests/unit/ContentHeader.spec.js
+++ src/tests/unit/ContentHeader.spec.js
@@ -5,5 +5,7 @@ describe('ContentHeader', () => {
   test('should render content correctly', () => {
     const wrapper = shallowMount(ContentHeader, {})
     expect(wrapper).not.toBeNull()
+    expect(wrapper.find('h1').text()).toEqual(expect.stringContaining('2020'))
+    expect(wrapper.find('img').exists()).toBe(true)
   })
 })
