d6833e8a99824c1b5ba4dc8b3cdf698404ebaac4
Sean Connelly <sconnel42@gmail.com>
2019-08-07 21:06:12 -0500

    Moar linting

--- server/app.js
+++ server/app.js
@@ -1,20 +1,19 @@
-const path = require('path');
-const express = require('express');
-const RSVP = require('./models/rsvp');
-const Twilio = require('twilio');
-const smsClient = new Twilio(process.env.TWILIO_API_KEY, process.env.TWILIO_API_SECRET);
-const history = require('connect-history-api-fallback');
-const app = express();
-const staticFileMiddleware = express.static(path.join(__dirname, '../dist'));
+const path = require('path')
+const express = require('express')
+const RSVP = require('./models/rsvp')
+const Twilio = require('twilio')
+const smsClient = new Twilio(process.env.TWILIO_API_KEY, process.env.TWILIO_API_SECRET)
+const history = require('connect-history-api-fallback')
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, '../dist'))
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(staticFileMiddleware);
+app.use(staticFileMiddleware)
 app.use(history({
-    index: '/dist/index.html'
-}));
-app.use(staticFileMiddleware);
-
+  index: '/dist/index.html'
+}))
+app.use(staticFileMiddleware)
 
 // Error handling
 // eslint-disable-next-line
@@ -23,56 +22,56 @@ app.use(function(err, req, res, next) {
   console.error(err.message);
 
   if (!err.statusCode) {
-		err.statusCode = 500;
-	}
-  
-	res.status(err.statusCode).send(err.message);
-});
+    err.statusCode = 500
+  }
+
+  res.status(err.statusCode).send(err.message)
+})
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '../dist') });
-});
+  res.sendFile('index.html', { root: path.join(__dirname, '../dist') })
+})
 
 // Put an RSVP into the DB
 app.post('/api/rsvp', (req, res, next) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const meal = req.body.meal;
-  const isComing = Boolean(req.body.isComing);
+  const name = req.body.name
+  const email = req.body.email
+  const meal = req.body.meal
+  const isComing = Boolean(req.body.isComing)
 
   if (!name || !email || !meal || req.body.isComing === undefined) {
-    let err = new Error('Missing parameter');
-		err.statusCode = 400;
-		return next(err);
+    let err = new Error('Missing parameter')
+    err.statusCode = 400
+    return next(err)
   }
 
   // Create a new rsvp
   RSVP.create({ name, email, meal, isComing }).then(
     // eslint-disable-next-line
     rsvp => {
-      return res.json({"message": `RSVP created with id: ${rsvp.id}`});
+      return res.json({ 'message': `RSVP created with id: ${rsvp.id}` })
     }
   ).catch(
     err => {
       // eslint-disable-next-line
       console.error(err.message);
 
-      let error = new Error('RSVP failed to be created!');
-      error.statusCode = 500;
-      return next(error);
+      let error = new Error('RSVP failed to be created!')
+      error.statusCode = 500
+      return next(error)
     }
-  );
-});
+  )
+})
 
 // Send a contact message
 app.post('/api/contact', (req, res, next) => {
-  const name = req.body.name;
-  const contactMessage = req.body.message;
+  const name = req.body.name
+  const contactMessage = req.body.message
 
   if (!name || !contactMessage) {
-    let err = new Error('Missing parameter');
-		err.statusCode = 400;
-		return next(err);
+    let err = new Error('Missing parameter')
+    err.statusCode = 400
+    return next(err)
   }
 
   smsClient.messages.create({
@@ -81,18 +80,18 @@ app.post('/api/contact', (req, res, next) => {
     body: `\n${contactMessage}\n\t-${name}`
   }).then(
     (message) => {
-      return res.json({"message": `Contact message sent with id ${message.sid}`});
+      return res.json({ 'message': `Contact message sent with id ${message.sid}` })
     }
   ).catch(
     (err) => {
-      let error = new Error('An error occurred while trying to send text!');
+      let error = new Error('An error occurred while trying to send text!')
 
       // eslint-disable-next-line
       console.error(err);
-      error.statusCode = 500;
-      return next(error);
+      error.statusCode = 500
+      return next(error)
     }
-  );
-});
+  )
+})
 
 module.exports = app
