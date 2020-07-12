33ec71cf07e52e59cde84d9935366f9725903a58
Sean Connelly <sconnel42@gmail.com>
2020-07-12 17:55:07 -0500

    Fix tests

--- src/tests/unit/Hotel.spec.js
+++ src/tests/unit/Hotel.spec.js
@@ -3,7 +3,11 @@ import Hotel from '../../components/Hotel.vue'
 
 describe('Hotel', () => {
   test('should render content correctly', () => {
-    const wrapper = shallowMount(Hotel, {})
+    const wrapper = shallowMount(Hotel, {
+      propsData: {
+        hotelEnabled: true
+      }
+    })
     expect(wrapper.exists()).toBe(true)
 
     let hotelInfo = wrapper.find('[id="hotel-info"]')
